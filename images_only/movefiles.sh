38517f8a6fc0a10a3e9e3cac30f054650bb1e9b1
alokmishra <mishralok@gmail.com>
2011-09-15 12:42:06 +0530

    added images to local path collecting them from some absolute path

---
+++ images_only/movefiles.sh
@@ -0,0 +1,51 @@
+cp /home/alok/phd/post_viva/semisup/spirals/100.eps                                                      semisup/spirals/100.eps                                              
+cp /home/alok/phd/post_viva/semisup/spirals/1.eps                                                        semisup/spirals/1.eps                                                    
+cp /home/alok/phd/post_viva/semisup/spirals/5.eps                                                        semisup/spirals/5.eps                                                    
+cp /home/alok/phd/post_viva/semisup/spirals/10.eps                                                       semisup/spirals/10.eps                                                   
+cp /home/alok/phd/post_viva/semisup/spirals/15.eps                                                       semisup/spirals/15.eps                                                   
+cp /home/alok/phd/post_viva/semisup/spirals/20.eps                                                       semisup/spirals/20.eps                                                   
+cp /home/alok/phd/post_viva/semisup/spirals/25.eps                                                       semisup/spirals/25.eps                                                   
+cp /home/alok/phd/post_viva/semisup/spirals/50.eps                                                       semisup/spirals/50.eps                                                   
+cp /home/alok/phd/post_viva/semisup/spirals/cv_fold1.eps                                                 semisup/spirals/cv_fold1.eps                                             
+cp /home/alok/phd/post_viva/semisup/spirals/cv_fold2.eps                                                 semisup/spirals/cv_fold2.eps                                             
+cp /home/alok/phd/post_viva/semisup/spirals/cv_fold3.eps                                                 semisup/spirals/cv_fold3.eps                                             
+cp /home/alok/phd/post_viva/semisup/spirals/cv_fold4.eps                                                 semisup/spirals/cv_fold4.eps                                             
+cp /home/alok/phd/post_viva/semisup/spirals/cv_fold5.eps                                                 semisup/spirals/cv_fold5.eps                                             
+                                                                                                                                                                                
+cp /home/alok/phd/post_viva/semisup/results/plots/stress_dunn.eps                                        semisup/results/plots/stress_dunn.eps                                    
+cp /home/alok/phd/post_viva/semisup/results/plots/stress_davies.eps                                      semisup/results/plots/stress_davies.eps                                  
+cp /home/alok/phd/post_viva/semisup/results/plots/ccycle_dunn.eps                                        semisup/results/plots/ccycle_dunn.eps                                    
+cp /home/alok/phd/post_viva/semisup/results/plots/ccycle_davies.eps                                      semisup/results/plots/ccycle_davies.eps                                  
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_only/1.eps                               semisup/results/analysis/img_stress_only/1.eps                           
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_only/2.eps                               semisup/results/analysis/img_stress_only/2.eps                           
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_chip/1.eps                               semisup/results/analysis/img_stress_chip/1.eps                           
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_chip/2.eps                               semisup/results/analysis/img_stress_chip/2.eps                           
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_ppi/1.eps                                semisup/results/analysis/img_stress_ppi/1.eps                            
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_ppi/2.eps                                semisup/results/analysis/img_stress_ppi/2.eps                            
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_yt/1.eps                                 semisup/results/analysis/img_stress_yt/1.eps                             
+cp /home/alok/phd/post_viva/semisup/results/analysis/img_stress_yt/2.eps                                 semisup/results/analysis/img_stress_yt/2.eps                             
+cp /home/alok/phd/post_viva/ec2machine/semisup/results/analysis/ccycle_only/1.eps                        ec2machine/semisup/results/analysis/ccycle_only/1.eps                    
+cp /home/alok/phd/post_viva/ec2machine/semisup/results/analysis/ccycle_ppi/1.eps                         ec2machine/semisup/results/analysis/ccycle_ppi/1.eps                     
+cp /home/alok/phd/post_viva/ec2machine/semisup/results/analysis/ccycle_ppi/2.eps                         ec2machine/semisup/results/analysis/ccycle_ppi/2.eps                     
+cp /home/alok/phd/post_viva/ec2machine/semisup/results/analysis/ccycle_chip/1.eps                        ec2machine/semisup/results/analysis/ccycle_chip/1.eps                    
+cp /home/alok/phd/post_viva/ec2machine/semisup/results/analysis/ccycle_chip/2.eps                        ec2machine/semisup/results/analysis/ccycle_chip/2.eps                    
+cp /home/alok/phd/post_viva/ec2machine/semisup/results/analysis/ccycle_only/1.eps                        ec2machine/semisup/results/analysis/ccycle_only/1.eps                    
+                                                                                                                                                                                
+                                                                                                                                                                                
+                                                                                                                                                                                
+                                                                                                                                                                                
+                                                                                                                                                                                
+cp /home/alok/phd/post_viva/maxent/results/sigmaopt/plots/chip_withinss.eps                              maxent/results/sigmaopt/plots/chip_withinss.eps                          
+cp /home/alok/phd/post_viva/maxent/results/sigmaopt/plots/ppi_withinss.eps                               maxent/results/sigmaopt/plots/ppi_withinss.eps                           
+cp /home/alok/phd/post_viva/maxent/results/sigmaopt/plots/yt_withinss.eps                                maxent/results/sigmaopt/plots/yt_withinss.eps                            
+cp /home/alok/phd/post_viva/maxent/results/analysis/img_maxent_stress_chip/1.eps                         maxent/results/analysis/img_maxent_stress_chip/1.eps                     
+cp /home/alok/phd/post_viva/maxent/results/analysis/img_maxent_stress_chip/2.eps                         maxent/results/analysis/img_maxent_stress_chip/2.eps                     
+cp /home/alok/phd/post_viva/maxent/results/analysis/img_maxent_stress_ppi/1.eps                          maxent/results/analysis/img_maxent_stress_ppi/1.eps                      
+cp /home/alok/phd/post_viva/maxent/results/analysis/img_maxent_stress_ppi/2.eps                          maxent/results/analysis/img_maxent_stress_ppi/2.eps                      
+cp /home/alok/phd/post_viva/maxent/results/analysis/img_maxent_stress_yt/1.eps                           maxent/results/analysis/img_maxent_stress_yt/1.eps                       
+cp /home/alok/phd/post_viva/maxent/results/analysis/img_maxent_stress_yt/2.eps                           maxent/results/analysis/img_maxent_stress_yt/2.eps                       
+cp /home/alok/phd/post_viva/ec2machine/maxent/results/analysis/img_maxent_ccycle_chip/1.eps              ec2machine/maxent/results/analysis/img_maxent_ccycle_chip/1.eps          
+cp /home/alok/phd/post_viva/ec2machine/maxent/results/analysis/img_maxent_ccycle_ppi/1.eps               ec2machine/maxent/results/analysis/img_maxent_ccycle_ppi/1.eps           
+cp /home/alok/phd/post_viva/ec2machine/maxent/results/analysis/img_maxent_ccycle_ppi/2.eps               ec2machine/maxent/results/analysis/img_maxent_ccycle_ppi/2.eps           
+cp /home/alok/phd/post_viva/ec2machine/maxent/results/analysis/img_maxent_ccycle_yt/1.eps                ec2machine/maxent/results/analysis/img_maxent_ccycle_yt/1.eps            
+cp /home/alok/phd/post_viva/ec2machine/maxent/results/analysis/img_maxent_ccycle_yt/2.eps                ec2machine/maxent/results/analysis/img_maxent_ccycle_yt/2.eps                 
